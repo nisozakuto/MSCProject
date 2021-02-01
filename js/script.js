9d2fdc340122bf427056ec5452e801fcf9b384e6
nisozakuto <nisozakuto@gmail.com>
2021-02-01 15:45:50 -0500

    Choose single color vs range

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ const magicButton = document.getElementById("getResult");
 let myLength = document.getElementById("length");
 const resultsBox = document.getElementsByClassName("results");
 let length = 100;
-
+let iscreateSecondDropdownCreated = false;
 let myString =
   "523453553235645235543675435564444424552454533353353553534524544355435245534322333323243233232333454344354424323322335424522334334343345553445466335463633552365322336335536556444535334344334342254524434253424522355452432545444443445244422433545252245453543343343462344465245342236324444325443445352233";
 let myArray = [];
@@ -28,6 +28,7 @@ function stringToArray(txt) {
 let myarticle = document.getElementById("article");
 function createAPixel(color) {}
 let myWidth = 100;
+
 function append() {
   let count = 1;
   while (count < 10) {
@@ -50,7 +51,6 @@ function append() {
       else if (myArray[i] == 7) pixel.id = "color7";
       else if (myArray[i] == 8) pixel.id = "color8";
       else if (myArray[i] == 9) pixel.id = "color9";
-      // pixel.style.backgroundColor = colors[myArray[i]];
       newImage.appendChild(pixel);
     }
     myWidth += 10;
@@ -58,9 +58,44 @@ function append() {
   }
 }
 
+function createDropdown() {
+  let selectColorValues = [];
+  let selectColor = document.createElement("select");
+  for (let i = 0; i < 1000; i++) {
+    selectColorValues[i] = i;
+  }
+  for (const val of selectColorValues) {
+    let option = document.createElement("option");
+    option.value = val;
+    option.text = val;
+    selectColor.appendChild(option);
+  }
+  selectColor.style.width = "70px";
+  selectColor.id = "chooseColorNumber";
+  document.getElementsByClassName("numbers")[0].append(selectColor);
+}
+
+function createSecondDropdown() {
+  let selectColorValues = [];
+  let selectColor = document.createElement("select");
+  for (let i = 0; i < 1000; i++) {
+    selectColorValues[i] = i;
+  }
+  for (const val of selectColorValues) {
+    let option = document.createElement("option");
+    option.value = val;
+    option.text = val;
+    selectColor.appendChild(option);
+  }
+  selectColor.style.width = "70px";
+  selectColor.id = "chooseColorNumber2";
+  document.getElementsByClassName("numbers")[0].append(selectColor);
+}
+
 function init() {
   stringToArray();
   getColors();
+  createDropdown();
 }
 let colorColor = [9];
 
@@ -87,6 +122,33 @@ const showColor8 = document.getElementsByClassName("showColor8");
 const showColor9 = document.getElementsByClassName("showColor9");
 
 let cssColor0 = document.getElementsByClassName("color0");
+
+const singleColorRadio = document.getElementById("singleColor");
+
+const rangeOfColorRadio = document.getElementById("rangeOfColor");
+singleColorRadio.addEventListener("click", () => {
+  if (rangeOfColorRadio) {
+    rangeOfColorRadio.checked = false;
+  }
+  if (document.getElementById("chooseColorNumber2")) {
+    console.log("alksj");
+    document.getElementById("chooseColorNumber2").style.display = "none";
+  }
+});
+
+rangeOfColorRadio.addEventListener("click", () => {
+  if (singleColorRadio) {
+    singleColorRadio.checked = false;
+  }
+  if (!iscreateSecondDropdownCreated) {
+    createSecondDropdown();
+    iscreateSecondDropdownCreated = true;
+  }
+  const chooseColorNumber2 = document.getElementById("chooseColorNumber2");
+  if ((chooseColorNumber2.style.display = "none")) {
+    chooseColorNumber2.style.display = "block";
+  }
+});
 
 const boxes = document.querySelectorAll(".box");
 
