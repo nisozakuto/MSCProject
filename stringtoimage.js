d6d072716f69b0ad68ae9c7c5906025051a960d4
nisozakuto <nisozakuto@gmail.com>
2021-03-30 21:58:09 -0400

    going through all the numbers and nulls for invert

--- stringtoimage.js
+++ stringtoimage.js
@@ -281,6 +281,8 @@ const createPicture = async (rowLength) => {
 
   let line, column;
 
+  console.log(invertedColors);
+
   if (isInverse) {
     (line = 0), (column = rowLength);
 
@@ -695,22 +697,24 @@ function setStringLengthText(length) {
 }
 
 function invertColor(hex) {
-  if (hex.indexOf("#") === 0) {
-    hex = hex.slice(1);
+  if (hex != null) {
+    if (hex.indexOf("#") === 0) {
+      hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length == 6) {
+      console.log(hex, "PROBLEM");
+    }
+    // invert color components
+    var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+      g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+      b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+    // pad each with zeros and return
+    return "#" + padZero(r) + padZero(g) + padZero(b);
   }
-  // convert 3-digit hex to 6-digits.
-  if (hex.length === 3) {
-    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-  }
-  if (hex.length !== 6) {
-    throw new Error("Invalid HEX color.");
-  }
-  // invert color components
-  var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
-    g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
-    b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
-  // pad each with zeros and return
-  return "#" + padZero(r) + padZero(g) + padZero(b);
 }
 
 function padZero(str, len) {
