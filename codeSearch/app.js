61638ea50849088474d48f6914a6d2caeb9a56fd
nisozakuto <nisozakuto@gmail.com>
2021-04-14 22:45:03 -0400

    Getting results in ejs

--- codeSearch/app.js
+++ codeSearch/app.js
@@ -2,14 +2,43 @@ const express = require("express");
 const app = express();
 const port = 3000;
 let myInput = [1, 3];
-let output = 0;
+const posts = [
+  {
+    id: 1,
+    author: "John",
+    title: "Templating with EJS",
+    body: "Blog post number 1",
+  },
+  {
+    id: 2,
+    author: "Drake",
+    title: "Express: Starting from the Bottom",
+    body: "Blog post number 2",
+  },
+  {
+    id: 3,
+    author: "Emma",
+    title: "Streams",
+    body: "Blog post number 3",
+  },
+  {
+    id: 4,
+    author: "Cody",
+    title: "Events",
+    body: "Blog post number 4",
+  },
+];
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
+  let output = 0;
+
   for (let i = 0; i < myInput.length; i++) {
     output += myInput[i];
     console.log(output);
   }
-  res.json(output);
+  //   res.json(output);
+  res.render("home", { output: output });
 });
 
 app.listen(port, () => {
