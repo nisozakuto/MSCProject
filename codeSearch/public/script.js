86bff270146df62f50145245e7edcdbab19506a7
nisozakuto <nisozakuto@gmail.com>
2021-04-26 21:54:01 -0400

    found some elements in js file

---
+++ codeSearch/public/script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    const forwardCheckbox = document.getElementById('forward')
+    const freverseCheckbox = document.getElementById('reverse')
+    const allvariationsCheckbox = document.getElementById('all-variations')
+    const searchButton = document.getElementById('searchButton')
+    console.log(searchButton)
+  };
