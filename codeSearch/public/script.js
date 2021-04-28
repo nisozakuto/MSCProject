3199641a7a689c470820a7175c7d4d345f5c3fdd
nisozakuto <nisozakuto@gmail.com>
2021-04-27 22:16:46 -0400

    Checking the checkboxes

--- codeSearch/public/script.js
+++ codeSearch/public/script.js
@@ -1,24 +1,41 @@
 window.onload = function () {
   const forwardCheckbox = document.getElementById("forward");
-  const freverseCheckbox = document.getElementById("reverse");
+  const reverseCheckbox = document.getElementById("reverse");
   const allvariationsCheckbox = document.getElementById("all-variations");
   const searchButton = document.getElementById("searchButton");
 
-  let canSearch = false;
-
   forwardCheckbox.addEventListener("change", () => {
-    if (forwardCheckbox.checked == true) {
-      console.log("false");
-      canSearch = true;
-    } else {
-      canSearch = false;
-    }
     checkCanSearch();
+    if (forwardCheckbox.checked && allvariationsCheckbox.checked) {
+      allvariationsCheckbox.checked = false;
+    }
+  });
+
+  reverseCheckbox.addEventListener("change", () => {
+    checkCanSearch();
+    if (reverseCheckbox.checked && allvariationsCheckbox.checked) {
+      allvariationsCheckbox.checked = false;
+    }
+  });
+
+  allvariationsCheckbox.addEventListener("change", () => {
+    checkCanSearch();
+    if (allvariationsCheckbox.checked) {
+      if (forwardCheckbox.checked || reverseCheckbox.checked) {
+        forwardCheckbox.checked = false;
+        reverseCheckbox.checked = false;
+      }
+    }
   });
 
   function checkCanSearch() {
-    if (canSearch) searchButton.disabled = false;
-    else searchButton.disabled = true;
+    if (
+      forwardCheckbox.checked ||
+      reverseCheckbox.checked ||
+      allvariationsCheckbox.checked
+    ) {
+      searchButton.disabled = false;
+    } else searchButton.disabled = true;
   }
 
   checkCanSearch();
