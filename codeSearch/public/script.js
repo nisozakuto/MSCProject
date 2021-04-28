05bbdae8f2b759d9d8dadfe1f0d960520027eae0
nisozakuto <nisozakuto@gmail.com>
2021-04-27 22:05:54 -0400

    Forward incrememnts checked checking

--- codeSearch/public/script.js
+++ codeSearch/public/script.js
@@ -1,7 +1,25 @@
 window.onload = function () {
-    const forwardCheckbox = document.getElementById('forward')
-    const freverseCheckbox = document.getElementById('reverse')
-    const allvariationsCheckbox = document.getElementById('all-variations')
-    const searchButton = document.getElementById('searchButton')
-    console.log(searchButton)
-  };
+  const forwardCheckbox = document.getElementById("forward");
+  const freverseCheckbox = document.getElementById("reverse");
+  const allvariationsCheckbox = document.getElementById("all-variations");
+  const searchButton = document.getElementById("searchButton");
+
+  let canSearch = false;
+
+  forwardCheckbox.addEventListener("change", () => {
+    if (forwardCheckbox.checked == true) {
+      console.log("false");
+      canSearch = true;
+    } else {
+      canSearch = false;
+    }
+    checkCanSearch();
+  });
+
+  function checkCanSearch() {
+    if (canSearch) searchButton.disabled = false;
+    else searchButton.disabled = true;
+  }
+
+  checkCanSearch();
+};
