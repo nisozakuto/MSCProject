d5807d22d5c4927787c19d1de0547a416ac0d91f
nisozakuto <nisozakuto@gmail.com>
2021-04-27 23:01:23 -0400

    increased the limits

--- codeSearch/server.js
+++ codeSearch/server.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 3000;
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+app.use(
+  bodyParser.json({ limit: "500mb", parameterLimit: 600000, extended: true })
+);
 
 app.set("view engine", "ejs");
 
@@ -19,8 +23,8 @@ app.get("/", (req, res) => {
 app.post("/getNumber", function (req, res) {
   let source = req.body.source;
   let target = req.body.target;
-  let incrementLower = req.body.incrementLower
-  let incremenetUpper = req.body.incremenetUpper
+  let incrementLower = req.body.incrementLower;
+  let incremenetUpper = req.body.incremenetUpper;
   let notes = [];
   let sourceArray = [];
   sourceArray = source.split("\r\n");
@@ -28,75 +32,102 @@ app.post("/getNumber", function (req, res) {
   let targetArray = [];
   targetArray = target.split(",");
 
-  let results = [], temp = [], didFind = false
-  
+  let results = [],
+    temp = [],
+    didFind = false;
+
   //Pushing 0 to all the indexes
-  for(let j = 0; j<sourceArray.length; j++)
-  {
-    results.push([sourceArray[j],0])
+  for (let j = 0; j < sourceArray.length; j++) {
+    results.push([sourceArray[j], 0]);
   }
   //i = incrememntValue
   //Going through all the increments
-  for(let incrememntValue = incrementLower; incrememntValue<=incremenetUpper; incrememntValue++)
-  {
+  for (
+    let incrememntValue = incrementLower;
+    incrememntValue <= incremenetUpper;
+    incrememntValue++
+  ) {
     //Going through the source indexes
-    for(let sourceIndex = 0; sourceIndex<sourceArray.length; sourceIndex++)
-    {
-      if(sourceArray[sourceIndex] == targetArray[0])
-      {
-        for(let targetCounter = 0; targetCounter<targetArray.length; targetCounter++)
-        {
-          if(sourceArray[sourceIndex+(targetCounter*incrememntValue)] == targetArray[targetCounter])
-          {
-            didFind= true
+    for (let sourceIndex = 0; sourceIndex < sourceArray.length; sourceIndex++) {
+      if (sourceArray[sourceIndex] == targetArray[0]) {
+        for (
+          let targetCounter = 0;
+          targetCounter < targetArray.length;
+          targetCounter++
+        ) {
+          if (
+            sourceArray[sourceIndex + targetCounter * incrememntValue] ==
+            targetArray[targetCounter]
+          ) {
+            didFind = true;
             //TO DO Adding the values to temp array
-          }
-          else {
-            didFind = false
-            break
+          } else {
+            didFind = false;
+            break;
           }
         }
-        if(didFind)
-        {
+        if (didFind) {
           //If Did Frind == true --> merge the temp values to results array.
-          for(let targetCounter = 0; targetCounter<targetArray.length; targetCounter++)
-          {
-            //1,000,002 --> Show why it is 1,000,002 -- It was 2 and it was 9 
+          for (
+            let targetCounter = 0;
+            targetCounter < targetArray.length;
+            targetCounter++
+          ) {
+            //1,000,002 --> Show why it is 1,000,002 -- It was 2 and it was 9
             //4th column - There was a skip of 2s and skip of 9. Target in this exmaple is 4 and 6
             // console.log(sourceIndex+(targetCounter*incrememntValue))
-            // console.log(results[sourceIndex+(targetCounter*incrememntValue)][0])    
+            // console.log(results[sourceIndex+(targetCounter*incrememntValue)][0])
             // console.log(`${sourceArray[sourceIndex+(targetCounter*incrememntValue)]} == ${targetArray[targetCounter]}`)
-            // results[j+(k*i)][1] = results[j+(k*i)][0] 
-            if(results[sourceIndex+(targetCounter*incrememntValue)][1] != 0)
-            {
-              notes.push( `Index ${incrememntValue} had: ${results[sourceIndex+(targetCounter*incrememntValue)][1]} \r\n`)
-              if(results[sourceIndex+(targetCounter*incrememntValue)][1] < 1000000) 
-              {results[sourceIndex+(targetCounter*incrememntValue)][1] = 1000001
-                notes.push( `Index ${incrememntValue} had: ${incrememntValue} \r\n`)
-              }
-              
-              if(results[sourceIndex+(targetCounter*incrememntValue)][1] > 1000000) 
-              {results[sourceIndex+(targetCounter*incrememntValue)][1] += 1
+            // results[j+(k*i)][1] = results[j+(k*i)][0]
+            if (
+              results[sourceIndex + targetCounter * incrememntValue][1] != 0
+            ) {
+              notes.push(
+                `Index ${incrememntValue} had: ${
+                  results[sourceIndex + targetCounter * incrememntValue][1]
+                } \r\n`
+              );
+              if (
+                results[sourceIndex + targetCounter * incrememntValue][1] <
+                1000000
+              ) {
+                results[
+                  sourceIndex + targetCounter * incrememntValue
+                ][1] = 1000001;
+                notes.push(
+                  `Index ${incrememntValue} had: ${incrememntValue} \r\n`
+                );
               }
 
-            }
-            else {
-              console.log(`Incr: ${incrememntValue} - Settting value to results[${sourceIndex+(targetCounter*incrememntValue)}]`)
-              results[sourceIndex+(targetCounter*incrememntValue)][1] = incrememntValue
+              if (
+                results[sourceIndex + targetCounter * incrememntValue][1] >
+                1000000
+              ) {
+                results[sourceIndex + targetCounter * incrememntValue][1] += 1;
+              }
+            } else {
+              console.log(
+                `Incr: ${incrememntValue} - Settting value to results[${
+                  sourceIndex + targetCounter * incrememntValue
+                }]`
+              );
+              results[
+                sourceIndex + targetCounter * incrememntValue
+              ][1] = incrememntValue;
             }
 
             // results[[j+(k*i)][0]] = sourceArray[j+(k*i)]
             // console.table("Line 92",results);
           }
           //Change the values for the corresponding items here
-          didFind = false
+          didFind = false;
         }
       }
     }
   }
   // console.table(results);
 
-  res.render("getNumber", { myData: results , notes});
+  res.render("getNumber", { myData: results, notes });
 });
 
 app.listen(PORT, () => {
