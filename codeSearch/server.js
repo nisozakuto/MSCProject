eddd08c87e66d5b7a8aebb3a17eccecadcac1c19
nisozakuto <nisozakuto@gmail.com>
2021-05-04 09:08:18 -0400

    Changed bodyparser

--- codeSearch/server.js
+++ codeSearch/server.js
@@ -4,11 +4,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false, }));
-app.use(bodyParser.json({ limit: "50mb", parameterLimit: 1000000, extended: true, }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+
 
 app.get("/", (req, res) => {
   let output = 0;
