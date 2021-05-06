5d25692a4364acbab76ff60bcb8256a01576a1e1
nisozakuto <nisozakuto@gmail.com>
2021-05-05 21:24:57 -0400

    Index column added

--- codeSearchJS/script.js
+++ codeSearchJS/script.js
@@ -29,60 +29,62 @@ function search() {
 
     for (let j = 0; j < sourceArray.length; j++) {
         results.push([j, sourceArray[j], 0]);
-        console.table(results)
     }
 
-    // for (let incrememntValue = incrementLower; incrememntValue <= incremenetUpper; incrememntValue++) {
-    //     // console.log(`Increment ${incrememntValue}`);
-    //     //Going through the source indexes
-    //     for (let sourceIndex = 0; sourceIndex < sourceArray.length; sourceIndex++) {
-    //         if (sourceArray[sourceIndex] == targetArray[0]) {
-    //             for (let targetCounter = 0; targetCounter < targetArray.length; targetCounter++) {
-    //                 if (sourceArray[sourceIndex + targetCounter * incrememntValue] == targetArray[targetCounter]) {
-    //                     didFind = true;
-    //                     //TO DO Adding the values to temp array
-    //                 }
-    //                 else {
-    //                     didFind = false;
-    //                     break;
-    //                 }
-    //             }
-    //             if (didFind) {
-    //                 for (let targetCounter = 0; targetCounter < targetArray.length; targetCounter++) {
-    //                     //1,000,002 --> Show why it is 1,000,002 -- It was 2 and it was 9
-    //                     //4th column - There was a skip of 2s and skip of 9. Target in this exmaple is 4 and 6
-    //                     // console.log(results[sourceIndex + targetCounter * incrememntValue][1])
-    //                     // console.log("UF", results[sourceIndex + targetCounter * incrememntValue][2], sourceIndex + targetCounter * incrememntValue)
+    for (let incrememntValue = incrementLower; incrememntValue <= incremenetUpper; incrememntValue++) {
+        // console.log(`Increment ${incrememntValue}`);
+        //Going through the source indexes
+        for (let sourceIndex = 0; sourceIndex < sourceArray.length; sourceIndex++) {
+            if (sourceArray[sourceIndex] == targetArray[0]) {
+                for (let targetCounter = 0; targetCounter < targetArray.length; targetCounter++) {
+                    if (sourceArray[sourceIndex + targetCounter * incrememntValue] == targetArray[targetCounter]) {
+                        didFind = true;
+                        //TO DO Adding the values to temp array
+                    }
+                    else {
+                        didFind = false;
+                        break;
+                    }
+                }
+                if (didFind) {
+                    for (let targetCounter = 0; targetCounter < targetArray.length; targetCounter++) {
 
-    //                     if (results[sourceIndex + targetCounter * incrememntValue][2] == undefined) { results[sourceIndex + targetCounter * incrememntValue][2] = `Index ${sourceIndex} skip of: ${incrememntValue} ` }
-    //                     else {
-    //                         results[sourceIndex + targetCounter * incrememntValue][2] += `Index ${sourceIndex} skip of: ${incrememntValue} `
-    //                     }
+                        //1,000,002 --> Show why it is 1,000,002 -- It was 2 and it was 9
+                        //4th column - There was a skip of 2s and skip of 9. Target in this exmaple is 4 and 6
+                        // console.log(results[sourceIndex + targetCounter * incrememntValue][1])
+                        // console.log("UF", results[sourceIndex + targetCounter * incrememntValue][2], sourceIndex + targetCounter * incrememntValue)
+                        console.log('results[targetCounter][sourceIndex + targetCounter * incrememntValue][2]', results[targetCounter])
 
-    //                     if (results[sourceIndex + targetCounter * incrememntValue][1] != 0) {
-    //                         // notes.push(`Index ${sourceIndex} had: ${incrememntValue}`);
-    //                         if (results[sourceIndex + targetCounter * incrememntValue][1] < 1000000) {
-    //                             results[sourceIndex + targetCounter * incrememntValue][1] = 1000001;
-    //                         }
+                        if (results[targetCounter, sourceIndex + targetCounter * incrememntValue][3] == undefined) { results[targetCounter, sourceIndex + targetCounter * incrememntValue][3] = `Index ${sourceIndex} skip of: ${incrememntValue} ` }
+                        else {
+                            results[targetCounter, sourceIndex + targetCounter * incrememntValue][3] += `Index ${sourceIndex} skip of: ${incrememntValue} `
+                        }
 
-    //                         if (results[sourceIndex + targetCounter * incrememntValue][1] > 1000000) {
-    //                             results[sourceIndex + targetCounter * incrememntValue][1] += 1;
-    //                         }
-    //                     }
-    //                     else {
-    //                         // console.log(`Incr: ${incrememntValue} - Settting value to results[${sourceIndex + targetCounter * incrememntValue}]`);
-    //                         results[sourceIndex + targetCounter * incrememntValue][1] = incrememntValue;
-    //                     }
+                        if (results[targetCounter, sourceIndex + targetCounter * incrememntValue][2] != 0) {
+                            // notes.push(`Index ${sourceIndex} had: ${incrememntValue}`);
+                            if (results[targetCounter, sourceIndex + targetCounter * incrememntValue][2] < 1000000) {
+                                results[targetCounter, sourceIndex + targetCounter * incrememntValue][2] = 1000001;
+                            }
 
-    //                     // results[[j+(k*i)][0]] = sourceArray[j+(k*i)]
-    //                     // console.table("Line 92",results);
-    //                 }
-    //                 //Change the values for the corresponding items here
-    //                 didFind = false;
-    //             }
-    //         }
-    //     }
-    // }
+                            if (results[targetCounter, sourceIndex + targetCounter * incrememntValue][2] > 1000000) {
+                                results[targetCounter, sourceIndex + targetCounter * incrememntValue][2] += 1;
+                            }
+                        }
+                        else {
+                            // console.log(`Incr: ${incrememntValue} - Settting value to results[${sourceIndex + targetCounter * incrememntValue}]`);
+                            results[targetCounter, sourceIndex + targetCounter * incrememntValue][2] = incrememntValue;
+                        }
+
+                        console.table(results)
+                        // results[[j+(k*i)][0]] = sourceArray[j+(k*i)]
+                        // console.table("Line 92",results);
+                    }
+                    //Change the values for the corresponding items here
+                    didFind = false;
+                }
+            }
+        }
+    }
     objectExporter({
         exportable: results, // The dataset to be exported form an array of objects, it can also be the DOM name for exporting DOM to html
         type: 'csv', // The type of exportable e.g. csv, xls or pdf
