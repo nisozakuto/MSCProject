e71c90cc9709b2a15d2a64f27b895d42c9abdc4d
nisozakuto <nisozakuto@gmail.com>
2021-05-05 21:53:49 -0400

    Last updated copied

--- codeSearchJS/script.js
+++ codeSearchJS/script.js
@@ -130,8 +130,28 @@ function search() {
 
 }
 
-function init() {
+function lastUpdatedFunction() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let repos = JSON.parse(this.responseText);
+            repos.forEach((repo) => {
+                if (repo.name == "MSCProject") {
+                    document.querySelector(
+                        "footer"
+                    ).innerText = `An MSC Webpage - Last updated ${new Date(
+                        repo.updated_at
+                    )}`;
+                }
+            });
+        }
+    };
+    xhttp.open("GET", "https://api.github.com/users/nisozakuto/repos", true);
+    xhttp.send();
+}
 
+function init() {
+    lastUpdatedFunction()
 }
 
 init()
